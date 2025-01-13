9f4437cd4f62292cae8b2c57fbbe98c5cc58dc9a
washington <lepexgamer102000@gmail.com>
2025-01-13 09:35:43 -0300

    feat: alterando a estrutura dos arquivos

---
+++ 1 - Sincronia de Funções/03-refatorando/script.js
@@ -0,0 +1,63 @@
+/*
+0 - Obter o usuário
+1 - Obter o num de telefone do usar apartir do ID
+2 - OTer o endereco pelo ID
+*/
+//Refatorando Callbacks para Promises
+
+//Promisse: vem no estado inicial pendding, (pendente) depois de resolvida vem no estado fullfilled (Sucesso), 
+// se der erro vem no estado rejected (Erro) e nós capturamos com try cat por exemplo.
+
+function obterUsuario(callback) {
+    setTimeout(() => {
+        return callback (null, {
+            id: 1,
+            nome: 'Aladin',
+            dataNascimento: new Date()
+        })
+    }, 1000)
+}
+
+function obterTelefone(idUsuario, callback) {
+    setTimeout(() => {
+    return callback(null, {
+        telefone: '1199002',
+        ddd: 11
+    })
+}, 2000)   
+}
+
+function obterEndereco(idUsuario, callback) {
+    setTimeout(() => {
+        return callback(null,{
+            rua: 'H1',
+            numero: 0
+        })
+    }, 3000)
+}
+
+obterUsuario(function resolverUsuario(error, usuario){
+    if(error){
+        console.error('Deu ruim', error);
+        return;
+    }
+    obterTelefone(usuario.id, function resolverTelefone(erro1, telefone) {
+    if (erro1){
+        console.error('Deu ruim', erro1);
+        return;
+    } 
+    
+    obterEndereco(usuario.id, function resolverEndereco(erro2, endereco){
+        if(erro2){
+            console.error('Deu ruim', erro2);
+            return;
+        }
+        console.log(`Nome: ${usuario.nome} Endereco: ${endereco.rua}, ${endereco.numero} Telefone: (${telefone.ddd})${telefone.telefone}`);
+        })
+    })
+})
+
+//const telefone = obterTelefone(usuario.id)
+
+//console.log('telefone', telefone);
+
